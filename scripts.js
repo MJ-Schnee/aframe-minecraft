b96ca8a6ecff49f1d7ee333c9b87bdefaafff5b1
MaskedPersona <michaeljaymes.schnee@gmail.com>
2019-05-11 20:23:50 -0400

    Updated isblock component to allow for multisrc texturing of blocks

--- scripts.js
+++ scripts.js
@@ -1,38 +1,40 @@
-var itemInHand = 0;
-/* Item IDs
-  bedrock = 0
-  dirt block = 1
-  grass block = 2
-*/
-
-/**
- *
-*/
 AFRAME.registerComponent('isblock', {
+  dependencies: ['position'],
+
   init: function(){
     const data = this.data;
     const el = this.el;
 
+    const src = data.src;
+    const srcTop = data.srcTop;
+    const srcBottom = data.srcBottom;
+    const srcSides = data.srcSides;
+
+    this.originalPos = el.getAttribute('position');
+
     el.setAttribute('geometry', "primitive: box");
-    el.setAttribute('material', 'src', data.src);
     el.setAttribute('material', 'material', 'flat');
-  }
-});
 
-/**
- * Snap entity to the closest interval specified by `snap`.
- */
-AFRAME.registerComponent('snap', {
-  dependencies: ['position'],
-
-  init: function(){
-    this.originalPos = this.el.getAttribute('position');
+    if(src != null)
+      el.setAttribute('material', 'src', data.src);
+    else {
+      el.setAttribute('multisrc', "");
+      // srcs: right, left, top, bottom, front, back
+      // srcs: 0,     1,    2,   3,      4,     5
+      let attributes = "src0:"+srcSides+";"+" src1:"+srcSides+";"+" src4:"+srcSides+";"+" src5:"+srcSides+";"
+      if(srcTop != null){
+        attributes.concat(" src2:"+srcTop+";");
+        attributes.concat(" src3:"+srcBottom);
+      }
+      el.setAttribute('multisrc', attributes);
+    }
   },
 
   update: function(){
     const pos = AFRAME.utils.clone(this.originalPos);
+
     pos.x = Math.floor(pos.x / .5) * .5;
-    pos.y = Math.floor(pos.y / .5) * .5;
+    pos.y = Math.floor(pos.y / .5) * .5 + .5;
     pos.z = Math.floor(pos.z / .5) * .5;
 
     this.el.setAttribute('position', pos);
---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 4000;
+
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
