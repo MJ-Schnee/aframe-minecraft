e533f90fb1de31901f867a4ccbbaa3a856c96e3c
MaskedPersona <michaeljaymes.schnee@gmail.com>
2019-05-29 10:34:55 -0400

    Commit for final day of class, desktop is functioning and updated README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # A-Frame Minecraft
-A VR Minecraft game made using A-Frame: A passion project for my VR class
+A WebVR passion project for my VR class based on Minecraft!
+Developed using A-Frame
+# Usage
+Controls
+* The player is default set to placing a block
+* The player cannot see the list of blocks but can see which block they are placing
+Button | Action
+------------ | -------------
+W | Move forward
+A | Move right
+S | Move left
+D | Move backward
+Spacebar | Fly upwards
+Left control | Fly downwards
+Left click | Place/break a block
+Tilde | Change between placing and breaking a block
+Equals | Increase in list of blocks to place
+Minus | Decrease in list of blocks to place
+# Local development
+git clone the repository
+If NPM or Python 3 are installed, you can run the server using those .bat files
--- scripts.js
+++ scripts.js
@@ -1,32 +1,98 @@
+// Player attributes
 var placingBlocks = true;
+var activeSprint = false;
+var flyingDwn = false;
+var flyingUpw = false;
+var changePlacingBlock = true;
+// Blocks
 var selectedBlock = 1;
 var listOfBlocks = [
   ["#bedrock"],
   ["#cobblestone"],
   ["#dirt"],
-  ["#grassBlockTop", "#drt", "#grassBlockSide"],
+  ["#grassBlockTop", "#dirt", "#grassBlockSide"],
   ["#sand"],
   ["#stone"],
   ["#woodBlockTop", "#woodBlockTop", "#woodBlockSide"],
   ["#woodenPlank"]
 ];
+// Keys
+var switchPlaceBreakBttn = 'Backquote'
+var increaseBlockBttn = 'Equal';
+var decreaseBlockBttn = 'Minus';
+var activeSprintBttn = 'ShiftLeft';
+var flyingDwnBttn = 'ControlLeft';
+var flyingUpwBttn = 'Space';
+
+window.addEventListener('keyup', function(evt){
+  switch(evt.code){
+    // Detecting sprint
+    case activeSprintBttn:
+      activeSprint = false;
+      break;
+    // Detecting flying up
+    case flyingUpwBttn:
+      flyingUpw = false;
+      break;
+    // Detecting flying down
+    case flyingDwnBttn:
+      flyingDwn = false;
+      break;
+  }
+});
+window.addEventListener('keydown', function(evt){
+  switch(evt.code){// Switching placing and blocking
+    case switchPlaceBreakBttn:
+      placingBlocks = !placingBlocks;
+      changePlacingBlock = true;
+      break;
+    // Swithcing placing item
+    case increaseBlockBttn:
+      selectedBlock++;
+      if(selectedBlock >= listOfBlocks.length)
+        selectedBlock = 0;
+      changePlacingBlock = true;
+      break;
+    case decreaseBlockBttn:
+      selectedBlock--;
+      if(selectedBlock <= 0)
+        selectedBlock = listOfBlocks.length-1;
+      changePlacingBlock = true;
+      break;
+    // Detecting sprint
+    case activeSprintBttn:
+      activeSprint = true;
+      break;
+    // Detecting flying up
+    case flyingUpwBttn:
+      flyingUpw = true;
+      break;
+    // Detecting flying down
+    case flyingDwnBttn:
+      flyingDwn = true;
+      break;
+  }
+});
 
 AFRAME.registerComponent('isblock', {
   dependencies: ['position'],
 
   init: function(){
-    const data = this.data;
     const el = this.el;
-
-    const src = data.src;
-    const srcTop = data.srcTop;
-    const srcBottom = data.srcBottom;
-    const srcSides = data.srcSides;
 
     this.originalPos = el.getAttribute('position');
 
     el.setAttribute('geometry', "primitive: box");
     el.setAttribute('material', 'material', 'flat');
+  },
+
+  update: function(){
+    const el = this.el;
+    const data = this.data;
+    const src = data.src;
+    const srcTop = data.srcTop;
+    const srcBottom = data.srcBottom;
+    const srcSides = data.srcSides;
 
     if(src != null)
       el.setAttribute('material', 'src', data.src);
@@ -37,16 +103,16 @@ AFRAME.registerComponent('isblock', {
       const attributes = `src0: ${srcSides}; src1: ${srcSides}; src2: ${srcTop}; src3: ${srcBottom}; src4: ${srcSides}; src5: ${srcSides}; `;
       el.setAttribute('multisrc', attributes);
     }
-  },
 
-  update: function(){
-    const pos = AFRAME.utils.clone(this.originalPos);
+    if(this.data.snappable == null){
+      const pos = AFRAME.utils.clone(this.originalPos);
 
-    pos.x = Math.floor(pos.x+.5);
-    pos.y = Math.floor(pos.y+.5);
-    pos.z = Math.floor(pos.z+.5);
+      pos.x = Math.floor(pos.x+.5);
+      pos.y = Math.floor(pos.y+.5);
+      pos.z = Math.floor(pos.z+.5);
 
-    this.el.setAttribute('position', pos);
+      this.el.setAttribute('position', pos);
+    }
   }
 });
 
@@ -87,9 +153,15 @@ AFRAME.registerComponent('blockmanipulator', {
         // Fixes user being unable to place block on left side of another block
         if(Math.abs(pos.x)%1==.5 && camera.getAttribute('rotation').y<=0)
           pos.x-=.5;
+        if(Math.abs(pos.z)%1==.5 &&
+            camera.getAttribute('rotation').y<=-90 ||camera.getAttribute('rotation').y>=90)
+          pos.z-=.5;
         // Prevents from (re)placing the "ground"
         if(pos.y<0)
           pos.y+=.5;
+        // Fixed user being unable to place underneath blocks
+        if(Math.abs(pos.y)%1==.5 && camera.getAttribute('rotation').x>0)
+          pos.y-=5;
         newBlock.setAttribute('position', pos);
         newBlock.setAttribute('isBlock', blockTexture);
         newBlock.setAttribute('breakable', "");
@@ -97,16 +169,54 @@ AFRAME.registerComponent('blockmanipulator', {
         scene.appendChild(newBlock);
       }
     });
-    this.el.addEventListener('keyup', function(evt){
-      console.log("onKeyUp:\n"+evt);
-      placingBlocks = !placingBlocks;
-    });
-    window.addEventListener('keyup', function(evt){
-      // Key Code 'r' = 82
-      if(evt.keyCode == 82){
-        placingBlocks = !placingBlocks;
-        console.log(`Placing Blocks = ${placingBlocks}`);
-      }
-    }, false);
+  }
+});
+
+AFRAME.registerComponent('playercontrol', {
+  tick: function(){
+    const rig = document.querySelector('#rig');
+    const blockInHand = document.querySelector('#blockInHand');
+
+    // Sprinting
+    if(activeSprint){
+      this.el.setAttribute('wasd-controls', 'acceleration', 230);
+      this.el.setAttribute('camera', 'fov', 90);
+    } else {
+      this.el.setAttribute('wasd-controls', 'acceleration', 150);
+      this.el.setAttribute('camera', 'fov', 80);
+    }
+    // Flying
+    if(flyingUpw)
+      rig.object3D.position.set(
+          rig.object3D.position.x,
+          rig.object3D.position.y+.05,
+          rig.object3D.position.z
+      );
+    else if(flyingDwn && rig.object3D.position.y>1.25)
+      rig.object3D.position.set(
+          rig.object3D.position.x,
+          rig.object3D.position.y-.05,
+          rig.object3D.position.z
+      );
+    // Change in block being placed
+    if(changePlacingBlock){
+      console.log("Change in placing blocks detected");
+      if(placingBlocks) {
+        blockInHand.setAttribute("visible", true);
+        // OPTIMIZE: Make a boolean value to detect if a change has occured to stop computer from constantly changing attribute
+        if(selectedBlock==3 || selectedBlock==6){
+          blockInHand.setAttribute("isblock", `srcTop: ${listOfBlocks[selectedBlock][0]};
+              srcSides: ${listOfBlocks[selectedBlock][2]};
+              srcBottom: ${listOfBlocks[selectedBlock][1]};
+              snappable: false`);
+          console.log(`${listOfBlocks[selectedBlock][0]}`);
+            }
+        else
+          blockInHand.setAttribute("isblock", `src: ${listOfBlocks[selectedBlock]};
+              snappable: false`);
+      } else
+        blockInHand.setAttribute("visible", false);
+      changePlacingBlock = false;
+    }
   }
 });
